2aa31cb536240f69e2774f796ed39b134023783a
Jujhar Singh <jujhar.pannu@gmail.com>
2022-10-21 13:49:44 -0700

    update ui

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,20 @@
 <script>
 
-let text = `The Anglo-Saxon Nine Herbs Charm mentions mucgwyrt. A folk etymology, based on coincidental sounds, derives mugwort from the word "mug"; more certainly, it has been used in flavoring drinks at least since the early Iron Age.[1] Other sources say mugwort is derived from the Old Norse muggi (meaning "marsh") and German wuertz (wort in English, originally meaning "root"), which refers to its use since ancient times to repel insects, especially moths.[2] The Old English word for mugwort is mucgwyrt where mucg-, could be a variation of the Old English word for "midge": mycg. Wort comes from the Old English wyrt (root/herb/plant), which is related to the Old High German wurz (root) and the Old Norse urt (plant).[3]
+let text = `Mugwort is a common name for several species of aromatic flowering plants in the genus Artemisia. In Europe, mugwort most often refers to the species Artemisia vulgaris, or common mugwort. In East Asia the species Artemisia argyi is often called "Chinese mugwort" in the context of traditional Chinese medicine, or àicǎo (艾草) in Mandarin. Artemisia princeps is a mugwort known in Korea as ssuk (쑥) and in Japan as yomogi (ヨモギ).[1] While other species are sometimes referred to by more specific common names, they may be called simply "mugwort" in many contexts.
+
+The Anglo-Saxon Nine Herbs Charm mentions mucgwyrt. A folk etymology, based on coincidental sounds, derives mugwort from the word "mug"; more certainly, it has been used in flavoring drinks at least since the early Iron Age.[1] Other sources say mugwort is derived from the Old Norse muggi (meaning "marsh") and German wuertz (wort in English, originally meaning "root"), which refers to its use since ancient times to repel insects, especially moths.[2] The Old English word for mugwort is mucgwyrt where mucg-, could be a variation of the Old English word for "midge": mycg. Wort comes from the Old English wyrt (root/herb/plant), which is related to the Old High German wurz (root) and the Old Norse urt (plant).[3]
 `;
 
 let nice = '';
 let state = 0;
-let display = '';
+let display = text.substring(0,24);
 let i = 0;
 let pos = 'normal'
 $: nice = text.split(' ');
 
 function renderText() {
+    window.scrollTo(0, 0);
+    document.body.style.cursor = `url('https://raw.githubusercontent.com/Silbad/pixa/main/icons/feather.svg'), auto`;
     if (i < 0) {
       i = 0;
     }
@@ -78,7 +82,9 @@ function handleKeydown(event) {
 <html data-theme="retro" lang="en">
 <body>
   <div id="wrapper" class="p-10">
-    <div>
+    <div style="padding: 300px;
+                padding-bottom: 200px;
+                font-size: 3em;">
     {display}
     </div>
 
@@ -87,7 +93,7 @@ function handleKeydown(event) {
     </div>
     <br />
 
-    <button on:click={renderText}>▶️</button>
+    <button id="play-btn" on:click={renderText}>▶️</button>
   </div>
 </body>
 </html>
@@ -97,11 +103,12 @@ function handleKeydown(event) {
 <style>
 body {
   max-width: 100%;
-  height: 850px;
+  height: 1450px;
   overflow-x: hidden;
   overflow-y: hidden;
-  background-color: #8527FF;
+  background-color: #100816;
   color: #FFFCFF;
+  text-shadow: black 3px 2px;
   font-size: 1.3em;
 }
 
@@ -116,8 +123,8 @@ body {
   background-color: turquoise;
   color: pink;
   font-size: 0.7em;
-  width: 300px;
-  height: 40px;
+  width: 1100px;
+  height: 282px;
 }
 
 #input {
